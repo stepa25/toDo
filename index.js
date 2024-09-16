d8b06e4aa7d72495b187da3abd32136fc883fde8
Stepa <stepakrutov030306@gmail.com>
2024-09-16 22:01:09 +0500

    second commit

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const inputValue = document.querySelector(".app__controls-input")
-const btn = document.querySelector(".app__controls-button")
-const tasksList = document.querySelector(".app__list")
+const todo = {
+    run() {
+        const HTML = localStorage.getItem("task")
 
-const addTask = (taskName) => {
-    const div = document.createElement("div")
-    const input = document.createElement("input")
-    const p = document.createElement("p")
-    const button = document.createElement("button")
-    const img = document.createElement("img")
-
-    div.className = "app__list-item"
-
-    input.type = "checkbox"
-    input.className = "app__list-checkbox"
-
-    p.className = "app__list-text"
-    p.textContent = taskName
-
-    img.src = "/assets/trash.svg"
-
-    div.appendChild(input)
-    div.appendChild(p)
-    div.appendChild(button)
-
-    button.appendChild(img)
-
-    div.addEventListener("click", () => {
-        if (div.className === "app__list-item") {
-            div.className = "app__list-item-completed"
-            input.checked = true
-        } else {
-            div.className = "app__list-item"
-            input.checked = false
+        if (HTML) {
+            document.querySelector(".app__list").innerHTML = HTML
         }
-    })
+        
+        document.addEventListener('click', this.onClick.bind(this))
+    },
+    createHTML(text) {
+        return `<div class="app__list-item">
+                    <input class="app__list-checkbox" type="checkbox">
+                    <p class="app__list-text">${text}</p>
+                    <button><img class="app__list-deleted" src="/assets/trash.svg" alt="trash"></button>
+                </div>`
+    },
+    addTask() { 
+        const inputText = document.querySelector(".app__controls-input")
 
-    button.addEventListener("click", (e) => {
-        tasksList.removeChild(div)
-    })
+        if (!inputText.value.length) return
 
-    tasksList.insertBefore(div, tasksList.firstChild)
+        document.querySelector(".app__list").insertAdjacentHTML('afterbegin', this.createHTML(inputText.value))
+        inputText.value = ''
+    },
+    onClick(e) {
+        if (e.target.className === "app__controls-button") {
+            this.addTask()
+            this.save()
+        } else if (e.target.className === "app__list-deleted") {
+            e.target.closest(".app__list-item").remove()
+            this.save()
+        } else if (e.target.closest(".app__list-item")) {
+            const taskItem = e.target.closest(".app__list-item");
+            const checkbox = taskItem.querySelector(".app__list-checkbox");
+
+            if (!taskItem.classList.contains("completed")) {
+                taskItem.classList.add("completed");
+                checkbox.checked = true;
+            } else {
+                taskItem.classList.remove("completed");
+                checkbox.checked = false;
+            }
+            this.save()
+        }
+
+    },
+    save() {
+        localStorage.setItem('task', document.querySelector(".app__list").innerHTML)
+    }
+
 }
 
-btn.addEventListener("click", () => {
-    if (inputValue.value === "") return
-    addTask(inputValue.value)
-    inputValue.value = ""
-})
+todo.run()
